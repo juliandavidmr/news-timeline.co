8b29bc6ad6768029c13e06b8a89c94baa533ce1e
juliandavidmr <julian.mora@crehana.com>
2021-04-28 19:17:55 -0500

    chore: add firebase hosting CI

---
+++ public/news-timeline.co.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    function ready(fn) {
+      if (document.readyState != 'loading'){
+        fn();
+      } else {
+        document.addEventListener('DOMContentLoaded', fn);
+      }
+    }
+
+    ready(() => {
+      const timeline = new TL.Timeline('timeline-embed',
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0d7Ijnow9m2B7rdCxJA4Oc83kHNYX7VYrF_Kfypx2zWLdXuagWRV9eaJWbcFVpsLMS-UGKv_3IowR/pubhtml?gid=0&single=true');
+    });
+
+}());
