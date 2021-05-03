5f0b45f161e746b62dbfceb4eaaf3de9f81e550e
juliandavidmr <julian.mora@crehana.com>
2021-05-03 08:58:37 -0500

    fix: start at end slides

--- public/news-timeline.co.js
+++ public/news-timeline.co.js
@@ -11,7 +11,11 @@
 
     ready(() => {
       const timeline = new TL.Timeline('timeline-embed',
-        'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0d7Ijnow9m2B7rdCxJA4Oc83kHNYX7VYrF_Kfypx2zWLdXuagWRV9eaJWbcFVpsLMS-UGKv_3IowR/pubhtml?gid=0&single=true');
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0d7Ijnow9m2B7rdCxJA4Oc83kHNYX7VYrF_Kfypx2zWLdXuagWRV9eaJWbcFVpsLMS-UGKv_3IowR/pubhtml?gid=0&single=true', {
+          language: 'es',
+          start_at_end: true,
+          language: 'es',
+        });
     });
 
 }());
